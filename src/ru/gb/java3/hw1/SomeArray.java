85775ab5f62005fb543f022807eccc4b8710e5b7
Nikolay N <nneshetkin@gmail.com>
2022-04-28 23:10:17 +0300

    1,2 task

--- src/ru/gb/java3/hw1/SomeArray.java
+++ src/ru/gb/java3/hw1/SomeArray.java
@@ -7,14 +7,14 @@ import java.util.List;
 
 public class SomeArray<T> {
 
-    public void changeIndex(T[] obj,int ind1,int ind2) {
+    public void changeIndex(T[] array,int ind1,int ind2) {
 
-        T a = obj[ind1];
-        obj[ind1] = obj[ind2];
-        obj[ind1] = a;
+        T a = array[ind1];
+        array[ind1] = array[ind2];
+        array[ind2] = a;
 
-        for (int i = 0; i < obj.length; i++) {
-            System.out.println(obj[i]);
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
         }
 
     }
@@ -28,9 +28,9 @@ public class SomeArray<T> {
         Integer[] i = {1, 3};
         Double[] d = {1.2, 3.1};
         SomeArray array = new SomeArray();
-        array.changeIndex(s,1,2);
-        array.changeIndex(i,1,2);
-        array.changeIndex(d,1,2);
+        array.changeIndex(s,0,1);
+        array.changeIndex(i,0,1);
+        array.changeIndex(d,0,1);
         System.out.println(array.toArrayList(s).getClass().getName());
 
     }
