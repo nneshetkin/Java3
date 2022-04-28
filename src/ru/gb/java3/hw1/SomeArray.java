6b76b33dfe2a8295bf11ab68ca1189584b07d26f
Nikolay N <nneshetkin@gmail.com>
2022-04-29 00:36:04 +0300

    HW1 ALL

--- src/ru/gb/java3/hw1/SomeArray.java
+++ src/ru/gb/java3/hw1/SomeArray.java
@@ -23,19 +23,6 @@ public class SomeArray<T> {
         return new ArrayList<>(Arrays.asList(array));
     }
 
-    public static void main(String[] args) {
-        String[] s = {"1", "2"};
-        Integer[] i = {1, 3};
-        Double[] d = {1.2, 3.1};
-        SomeArray array = new SomeArray();
-        array.changeIndex(s,0,1);
-        array.changeIndex(i,0,1);
-        array.changeIndex(d,0,1);
-        System.out.println(array.toArrayList(s).getClass().getName());
-
-    }
-
-
 }
 
 
